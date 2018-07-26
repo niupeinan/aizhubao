6be291478d10f39f2e79ba2fefa7b7cee429c4db
Your Name <867761374@qq.com>
2018-07-26 23:28:50 +0800

    2

--- js/index.js
+++ js/index.js
@@ -2,5 +2,18 @@
 * @Author: 牛培楠
 * @Date:   2018-07-17 14:57:17
 * @Last Modified by:   牛培楠
-* @Last Modified time: 2018-07-17 14:58:15
+* @Last Modified time: 2018-07-23 22:15:51
 */
+// window.onload=function(){
+// 	let audio=document.getElementsByClassName("audio")[0];
+// 	let header=audio.getElementsByClassName("header")[0];
+// 	let lis=header.getElementsByTagName("a")[0];
+// 	let span=header.getElementsByTagName("span")[0];
+// 	console.log(audio,header,lis,span);
+// 	lis.onmouseenter=function(){
+// 		span.innerText="HOME";
+// 	}
+// 	lis.onmouseleave=function(){
+// 		span.innerText="首页";
+// 	}
+// }
